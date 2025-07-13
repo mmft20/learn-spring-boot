6b0b12614400fe0e3169e23e6884762c1655ed75
Rajat Dhingra <rajatdhingra_rd@yahoo.com>
2025-07-14 00:43:17 +0800

    LearnSprintBoot : Commit : 001 Date   : 2025-07-14 MileSt.: YES Lecture: 11 Details: First Commit. -- Created Spring Project. -- Started Spring Project. -- Created First REST controller

---
+++ src/main/java/com/in28minutes/springboot/learn_spring_boot/CourseController.java
@@ -0,0 +1,20 @@
+package com.in28minutes.springboot.learn_spring_boot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CourseController {
+
+    @RequestMapping("/courses")
+    public List<Course> retrieveAllCourses(){
+        return Arrays.asList(
+                new Course(1, "Java","Rajat"),
+                new Course(2,"Python","Rajat")
+        );
+
+    }
+}
